a4b2df909602970d9614b2052f9d3334ebe31a9f
andrzej <andrzej.czabajski@collegiumwitelona.pl>
2023-06-19 23:47:50 +0200

    zadanie 2 operacje arytmetyczne

--- 0007 PPI Wykład 2/Zadanie2/Zadanie2.c
+++ 0007 PPI Wykład 2/Zadanie2/Zadanie2.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 int main(int argc, char* argv[])
 {
-    int a, b, wynik1;
-    double x, y, wynik2;
+    int a, b, sum1;
+    double x, y, sum2;
     system("chcp 1250 > nul"); // strona kodowa Windows 1250
     puts("Operacje arytmetyczne na liczbach typu 'int'");
     fflush(stdin); // usuń znaki z bufora klawiatury
@@ -11,18 +11,18 @@ int main(int argc, char* argv[])
     scanf("%d", &a); // uwaga na operator &
     printf("b= ");
     scanf("%d", &b);
-    wynik1 = a + b;
-    printf("%d + %d = %d\n", a, b, wynik1);
-    wynik1 = a - b;
-    printf("%d - %d = %d\n", a, b, wynik1);
-    wynik1 = a * b;
-    printf("%d * %d = %d\n", a, b, wynik1);
+    sum1 = a + b;
+    printf("%d + %d = %d\n", a, b, sum1);
+    sum1 = a - b;
+    printf("%d - %d = %d\n", a, b, sum1);
+    sum1 = a * b;
+    printf("%d * %d = %d\n", a, b, sum1);
     if (b != 0)
     { // warunek popr. wykonania operacji
-        wynik1 = a / b;
-        printf("%d / %d = %d\n", a, b, wynik1);
-        wynik1 = a % b;
-        printf("%d %c %d = %d\n", a, '%', b, wynik1); //uwaga
+        sum1 = a / b;
+        printf("%d / %d = %d\n", a, b, sum1);
+        sum1 = a % b;
+        printf("%d %c %d = %d\n", a, '%', b, sum1); //uwaga
     }
     puts("\nOperacje arytmetyczne na liczbach typu 'double'");
     fflush(stdin); // usuń znaki z bufora klawiatury
@@ -30,16 +30,16 @@ int main(int argc, char* argv[])
     scanf("%lf", &x);
     printf("y= ");
     scanf("%lf", &y);
-    wynik2 = x + y;
-    printf("%.2lf + %.2lf = %.2lf\n", x, y, wynik2);
-    wynik2 = x - y;
-    printf("%.2lf - %.2lf = %.2lf\n", x, y, wynik2);
-    wynik2 = x * y;
-    printf("%.2lf * %.2lf = %.2lf\n", x, y, wynik2);
+    sum2 = x + y;
+    printf("%.2lf + %.2lf = %.2lf\n", x, y, sum2);
+    sum2 = x - y;
+    printf("%.2lf - %.2lf = %.2lf\n", x, y, sum2);
+    sum2 = x * y;
+    printf("%.2lf * %.2lf = %.2lf\n", x, y, sum2);
     if (y != 0.0)
     { // warunek popr. wykonania operacji
-        wynik2 = x / y;
-        printf("%.2lf / %.2lf = %.2f\n", x, y, wynik2);
+        sum2 = x / y;
+        printf("%.2lf / %.2lf = %.2f\n", x, y, sum2);
     }
     system("pause");
     return 0;

415ed4adfbba6560fbc2a6ed509cba77bebc46be
andrzej <andrzej.czabajski@collegiumwitelona.pl>
2023-06-20 22:42:42 +0200

    dodano warunek (y==5.0)

--- 0007 PPI Wykład 2/Zadanie2/Zadanie2.c
+++ 0007 PPI Wykład 2/Zadanie2/Zadanie2.c
@@ -41,10 +41,10 @@ int main(int argc, char* argv[])
         sum2 = x / y;
         printf("%.2lf / %.2lf = %.2f\n", x, y, sum2);
     }
-    if (y=5.0)
+    if (y==5.0)
     {
         sum2 = x * x * y;
-           printf("%.2lf * %.2lf * %2.lf = %.2lf\n", x,x,y,sum2);
+           printf("%.2lf * %.2lf * %.2lf = %.2lf\n", x,x,y,sum2);
     }
     system("pause");
     return 0;

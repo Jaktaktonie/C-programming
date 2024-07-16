b965225b6864ef52c0de70324980e810ffc52638
andrzej <andrzej.czabajski@collegiumwitelona.pl>
2024-07-16 23:44:18 +0200

    testy

--- 0008 iteruje/Main.c
+++ 0008 iteruje/Main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Witaj świecie!\n");
+    // Uncomment the line below if you need to set the code page to UTF-8 for Windows console.
+    // system("chcp 65001");
+
+    int a, b, suma;
+    printf("Początkowe wartości: a = %d, b = %d\n", a, b); // Wypisanie początkowych wartości
+    suma = a + b;
+
+    printf("Wpisz pierwszą liczbę:\n");
+    scanf("%d", &a);
+    printf("Wpisz drugą liczbę:\n");
+    scanf("%d", &b);
+
+    printf("Wynik dodawania = %d\n", suma);
+
     return 0;
 }
